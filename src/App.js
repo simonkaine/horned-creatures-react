ea93060c8b5565529f53fbec0d3837be1b81fc8e
Simon Kaine <simonkaine@gmail.com>
2021-08-03 14:56:28 -0700

    Built dropdown list

--- src/App.js
+++ src/App.js
@@ -4,11 +4,33 @@ import ImageList from './imageList.jsx';
 
 class App extends Component {
   state = { 
-   
+      keyword: 'All'
    }
   render() { 
     return ( 
       <div className='App'>
+          <select>
+              <option value="narwhal">Uniwhal</option>
+              <option value="rhino">Rhino Family</option>
+              <option value="unicorn">Unicorn Head</option>
+              <option value="unilego">UniLego</option>
+              <option value="narwhal">Basically a unicorn</option>
+              <option value="narwhal">#truth</option>
+              <option value="rhino">Baby Rhino</option>
+              <option value="triceratops">Cera</option>
+              <option value="narwhal">Narwhal costume</option>
+              <option value="rhino">Rhino costume</option>
+              <option value="unicorn">Believe</option>
+              <option value="markhor">Markhor</option>
+              <option value="markhor">Baby markhor</option>
+              <option value="mouflon">Mouflon</option>
+              <option value="addax">Addax</option>
+              <option value="mouflon">Baby mouflon</option>
+              <option value="chameleon">Happy Jackson's Chameleon</option>
+              <option value="chameleon">Serious Jackson's Chameleon</option>
+              <option value="lizard">Horned Lizard</option>
+              <option value="dragon">Smaug</option>
+          </select>
           <ImageList images={images} />
       </div>
      );
--- src/imageList.jsx
+++ src/imageList.jsx
@@ -4,10 +4,9 @@ import ImageItem from './ImageItem.jsx';
 class ImageList extends Component {
     state = {  }
     render() { 
-        console.log(this.props.images)
         return ( 
             <>
-            <h1>Image list page rendering</h1>
+            <h1>Cute Adorable Cuddly Characters</h1>
                 {this.props.images.map((item) => {
                     return <ImageItem key={item.title} images={item}/>
                 })} 
